92e99125aac0437c29cd137b4caeb8ecf8644c26
Ian Jacobi <jacobi@csail.mit.edu>
2011-09-12 13:49:49 -0400

    Add updated server_cgi.py here too.

--- server_cgi.py
+++ server_cgi.py
@@ -4,11 +4,16 @@
 An attempt to get a cgi interface to policyrunner.py
 
 """
-DEBUG = TRUE
+DEBUG = False
 
 ctype = 'text/rdf+n3'
 
+if DEBUG:
+    ctype = 'text/plain'
+
 import cgi
+import urllib
+import traceback
 # instead of cgi.FieldStorage, can we use  paste.wsgiwrappers.WSGIRequest(env).params, 
 # as documented in
 # http://pythonpaste.org/class-paste.wsgiwrappers.WSGIRequest.html and 
@@ -45,21 +50,32 @@ def wsgi_app(environ, start_response):
     ruleURIs = form.getlist('rulesFile')
     log = form.getfirst('log')
     rules = form.getfirst('policy')
-    from tmswap import policyrunner
-    testPolicy = policyrunner.testPolicy
-    returnString = testPolicy(logURIs, ruleURIs, log, rules)
-    returnString = returnString.encode('utf_8')
-    print ('ran testPolicy(%s, %s, %r, %r)\n' % (logURIs, ruleURIs, log, rules))
-    print (str(form.keys()) + '\n')
-    if DEBUG:
-        length = str(len(returnString) + len(sys.stdout.getvalue()))
-    else:
-        length =  str(len(returnString))
-    start_response('200 OK', [('Content-Type',ctype),
-                              ("Content-Length", length)])
-    if DEBUG:
-        yield sys.stdout.getvalue()
-    yield returnString
+    filterProperties = form.getlist('filterProperties')
+    if len(filterProperties) == 0:
+        filterProperties = ['http://dig.csail.mit.edu/TAMI/2007/amord/air#compliant-with', 'http://dig.csail.mit.edu/TAMI/2007/amord/air#non-compliant-with']
+    import policyrunner as amord
+#    from tmswap import amord
+    testPolicy = amord.testPolicy
+    try:
+        returnString = testPolicy(logURIs, ruleURIs, log, rules, filterProperties)
+        returnString = returnString.encode('utf_8')
+        print ('ran testPolicy(%s, %s, %r, %r)\n' % (logURIs, ruleURIs, log, rules))
+        print (str(form.keys()) + '\n')
+        if DEBUG:
+            length = str(len(returnString) + len(sys.stdout.getvalue()))
+        else:
+            length =  str(len(returnString))
+        start_response('200 OK', [('Content-Type',ctype),
+                                  ("Content-Length", length)])
+        if DEBUG:
+            yield sys.stdout.getvalue()
+        yield returnString
+    except:
+        returnString = ''.join(traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback))
+        length = str(len(returnString))
+        start_response('500 Internal Server Error', [('Content-Type','text/plain'),
+                                                     ("Content-Length", length)])
+        yield returnString
     sys.stdout = outfile
 
 
@@ -83,25 +99,38 @@ def main():
     ruleURIs = form.getlist('rulesFile')
     log = form.getfirst('log')
     rules = form.getfirst('policy')
-    from tmswap import policyrunner
-    testPolicy = policyrunner.testPolicy
-    returnString = testPolicy(logURIs, ruleURIs, log, rules)
-    returnString = returnString.encode('utf_8')
-    print ('ran testPolicy(%s, %s, %r, %r)\n' % (logURIs, ruleURIs, log, rules))
-    print (str(form.keys()) + '\n')
-    send_header(outfile, "Content-type", ctype)
-    if DEBUG:
-        length = str(len(returnString) + len(sys.stdout.getvalue()))
-    else:
-        length =  str(len(returnString))
-    send_header(outfile, "Content-Length", length)
-#    print ctype
-    end_headers(outfile)
-    if DEBUG:
-        outfile.write(sys.stdout.getvalue())
-    outfile.write(returnString)
+    filterProperties = form.getlist('filterProperties')
+    if len(filterProperties) == 0:
+        filterProperties = ['http://dig.csail.mit.edu/TAMI/2007/amord/air#compliant-with', 'http://dig.csail.mit.edu/TAMI/2007/amord/air#non-compliant-with']
+    import policyrunner as amord
+    testPolicy = amord.testPolicy
+    try:
+        returnString = testPolicy(logURIs, ruleURIs, log, rules, filterProperties)
+        returnString = returnString.encode('utf_8')
+        print ('ran testPolicy(%s, %s, %r, %r, %s)\n' % (logURIs, ruleURIs, log, rules, filterProperties))
+        print (str(form.keys()) + '\n')
+        send_header(outfile, "Content-type", ctype)
+        if DEBUG:
+            length = str(len(returnString) + len(sys.stdout.getvalue()))
+        else:
+            length =  str(len(returnString))
+        send_header(outfile, "Content-Length", length)
+#        print ctype
+        end_headers(outfile)
+        if DEBUG:
+            outfile.write(sys.stdout.getvalue())
+        outfile.write(returnString)
+    except:
+        returnString = ''.join(traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback))
+        send_header(outfile, "Content-type", 'text/plain')
+        length = str(len(returnString))
+        send_header(outfile, "Content-Length", length)
+        end_headers(outfile)
+#        start_response('500 Internal Server Error', [('Content-Type','text/plain'),
+#                                                     ("Content-Length", length)])
+        outfile.write(returnString)
     sys.stdout = outfile
 
 
 if __name__ == '__main__': # What else would it be?
-    main()
+    main_wsgi()
